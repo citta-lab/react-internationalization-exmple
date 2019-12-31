f60d084f2927840162cac3736c5520339bd44148
Mahesh <mahesh2684@gmail.com>
2019-12-30 23:18:40 -0500

    making use of map for better card use

--- internationalized-app/src/components/cards/CardManager.js
+++ internationalized-app/src/components/cards/CardManager.js
@@ -3,28 +3,44 @@ import React from 'react'
 import Card from './Card';
 
 const CardManager = () => {
+
+    const cardData = [
+        {
+            number: 1,
+            type: 'Number',
+            description: 'Displaying 2345678187243 in regional format'
+        },
+        {
+            number: '2',
+            type: 'Date',
+            description: 'Displaying date 09/12/2019 in regional format'
+        },
+        {
+            number: '3',
+            type: 'String',
+            description: 'Such as web services, scripts, configurations'
+        },
+        {
+            number: '4',
+            type: 'Plural',
+            description: 'Web projects and ensure the quality of delivery'
+        }
+    ];
+
+    const cardList = cardData.map(card => {
+        return (
+            <Card
+                key={card.number}
+                number={card.number}
+                type={card.type}
+                description={card.description}
+            />
+        )
+    })
+
     return (
         <div className="container ">
-            <Card
-                number='1'
-                type='Number'
-                description='Displaying 2345678187243 in regional format'
-            />
-            <Card
-                number='2'
-                type='Date'
-                description='Displaying date 09/12/2019 in regional format'
-            />
-            <Card
-                number='3'
-                type='String'
-                description='Such as web services, scripts, configurations'
-            />
-            <Card
-                number='4'
-                type='Plural'
-                description='Web projects and ensure the quality of delivery'
-            />
+            {cardList}
         </div>
     )
 }
